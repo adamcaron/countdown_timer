a60623a5c6488e15b76a16b1de688a2f3f8d069c
Adam Caron <adamwcaron@gmail.com>
2015-10-13 10:27:11 -0600

    Countdown to zero from ?

--- countdown.js
+++ countdown.js
@@ -1,5 +1,13 @@
-function countdown(seconds){
-  // ...
+function countdown(seconds, callback){
+  var interval = setInterval(function() {
+    if (seconds >= 0) {
+      callback(seconds--)
+    } else {
+      clearInterval(interval);
+    }
+  }, 1000);
 }
 
-countdown(5);
+countdown(5, function (s) {
+  console.log(s);
+});
